affc46ff50a079fad6af0db78a08be285933cc43
Corn <SCorn@zebra.com>
2017-08-08 15:36:37 -0500

    Initial ScanPairDemo_VB.NET project commit:

---
+++ ScanPairDemo_VB.NET/BluetoothScanPair_VB/BluetoothScanPairVBNETForm.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Public Class BluetoothScanPairVBNETForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+    Private mainMenu1 As System.Windows.Forms.MainMenu
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.mainMenu1 = New System.Windows.Forms.MainMenu
+        Me.statusLabel = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.macAddrBox = New System.Windows.Forms.TextBox
+        Me.pairButton = New System.Windows.Forms.Button
+        Me.unpairButton = New System.Windows.Forms.Button
+        Me.sendTxtBox = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.sendFileBox = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.browseButton = New System.Windows.Forms.Button
+        Me.quitButton = New System.Windows.Forms.Button
+        Me.sendFileButton = New System.Windows.Forms.Button
+        Me.sendTxtButton = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'statusLabel
+        '
+        Me.statusLabel.BackColor = System.Drawing.Color.Red
+        Me.statusLabel.Location = New System.Drawing.Point(0, 0)
+        Me.statusLabel.Name = "statusLabel"
+        Me.statusLabel.Size = New System.Drawing.Size(240, 20)
+        Me.statusLabel.Text = "Status: Unpaired"
+        Me.statusLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(0, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(240, 20)
+        Me.Label2.Text = "Scan the MAC Address and Click Pair:"
+        '
+        'macAddrBox
+        '
+        Me.macAddrBox.Location = New System.Drawing.Point(3, 44)
+        Me.macAddrBox.Name = "macAddrBox"
+        Me.macAddrBox.Size = New System.Drawing.Size(234, 21)
+        Me.macAddrBox.TabIndex = 2
+        '
+        'pairButton
+        '
+        Me.pairButton.Location = New System.Drawing.Point(3, 68)
+        Me.pairButton.Name = "pairButton"
+        Me.pairButton.Size = New System.Drawing.Size(72, 20)
+        Me.pairButton.TabIndex = 3
+        Me.pairButton.Text = "Pair"
+        '
+        'unpairButton
+        '
+        Me.unpairButton.Location = New System.Drawing.Point(165, 68)
+        Me.unpairButton.Name = "unpairButton"
+        Me.unpairButton.Size = New System.Drawing.Size(72, 20)
+        Me.unpairButton.TabIndex = 4
+        Me.unpairButton.Text = "Unpair"
+        '
+        'sendTxtBox
+        '
+        Me.sendTxtBox.Location = New System.Drawing.Point(3, 118)
+        Me.sendTxtBox.Name = "sendTxtBox"
+        Me.sendTxtBox.Size = New System.Drawing.Size(234, 21)
+        Me.sendTxtBox.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(0, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(240, 20)
+        Me.Label3.Text = "Send plain text to device:"
+        '
+        'sendFileBox
+        '
+        Me.sendFileBox.BackColor = System.Drawing.SystemColors.Info
+        Me.sendFileBox.Location = New System.Drawing.Point(3, 195)
+        Me.sendFileBox.Name = "sendFileBox"
+        Me.sendFileBox.Size = New System.Drawing.Size(234, 21)
+        Me.sendFileBox.TabIndex = 11
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(0, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(240, 20)
+        Me.Label4.Text = "Send file to device:"
+        '
+        'browseButton
+        '
+        Me.browseButton.Location = New System.Drawing.Point(3, 223)
+        Me.browseButton.Name = "browseButton"
+        Me.browseButton.Size = New System.Drawing.Size(72, 20)
+        Me.browseButton.TabIndex = 13
+        Me.browseButton.Text = "Browse"
+        '
+        'quitButton
+        '
+        Me.quitButton.Location = New System.Drawing.Point(165, 223)
+        Me.quitButton.Name = "quitButton"
+        Me.quitButton.Size = New System.Drawing.Size(72, 20)
+        Me.quitButton.TabIndex = 15
+        Me.quitButton.Text = "Quit"
+        '
+        'sendFileButton
+        '
+        Me.sendFileButton.Location = New System.Drawing.Point(84, 223)
+        Me.sendFileButton.Name = "sendFileButton"
+        Me.sendFileButton.Size = New System.Drawing.Size(72, 20)
+        Me.sendFileButton.TabIndex = 14
+        Me.sendFileButton.Text = "Send File"
+        '
+        'sendTxtButton
+        '
+        Me.sendTxtButton.Location = New System.Drawing.Point(165, 145)
+        Me.sendTxtButton.Name = "sendTxtButton"
+        Me.sendTxtButton.Size = New System.Drawing.Size(72, 20)
+        Me.sendTxtButton.TabIndex = 20
+        Me.sendTxtButton.Text = "Send Text"
+        '
+        'BluetoothScanPairVBForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.ClientSize = New System.Drawing.Size(240, 268)
+        Me.Controls.Add(Me.sendTxtButton)
+        Me.Controls.Add(Me.quitButton)
+        Me.Controls.Add(Me.sendFileButton)
+        Me.Controls.Add(Me.browseButton)
+        Me.Controls.Add(Me.sendFileBox)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.sendTxtBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.unpairButton)
+        Me.Controls.Add(Me.pairButton)
+        Me.Controls.Add(Me.macAddrBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.statusLabel)
+        Me.Menu = Me.mainMenu1
+        Me.MinimizeBox = False
+        Me.Name = "BluetoothScanPairVBForm"
+        Me.Text = "Bluetooth Scan and Pair"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents statusLabel As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents macAddrBox As System.Windows.Forms.TextBox
+    Friend WithEvents pairButton As System.Windows.Forms.Button
+    Friend WithEvents unpairButton As System.Windows.Forms.Button
+    Friend WithEvents sendTxtBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents sendFileBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents browseButton As System.Windows.Forms.Button
+    Friend WithEvents quitButton As System.Windows.Forms.Button
+    Friend WithEvents sendFileButton As System.Windows.Forms.Button
+    Friend WithEvents sendTxtButton As System.Windows.Forms.Button
+
+End Class
---
+++ ScanPairDemo_VB.NET/BluetoothScanPair_VB/BluetoothScanPairVBNETForm.vb
@@ -0,0 +1,278 @@
+﻿'
+' CONFIDENTIAL AND PROPRIETARY 
+'
+' The source code and other information contained herein is the confidential and the exclusive property of
+' Zebra Technologies Corporation and is subject to the terms and conditions in your end user license agreement.
+' This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+' displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+' expressly permitted under such license agreement.
+' 
+' Copyright Zebra Technologies Corporation 2010
+'
+' ALL RIGHTS RESERVED 
+'
+' File:   BluetoothScanPairVBNETForm.vb
+'
+'Descr:  This demo application allows the user to enter a MAC address and then pairs the zebra printer 
+'to the windows mobile device using MAC address.
+'
+' Date: 03/16/2011 
+
+Imports ZSDK_API.Comm
+Imports ZSDK_API.ApiException
+Imports ZSDK_API.Util.Internal
+Imports System.IO
+' Commented code of Symbol.dll
+'Imports Symbol
+'Imports Symbol.Barcode
+
+Public Class BluetoothScanPairVBNETForm
+    Private connection As ZebraPrinterConnection
+    Private isConnected As Boolean = False
+
+    ' Commented code of Symbol.dll
+
+    'Private MyReader As Symbol.Barcode.Reader = Nothing
+    'Private MyReaderData As Symbol.Barcode.ReaderData = Nothing
+    'Private MyEventHandler As System.EventHandler = Nothing
+
+    ' The following method is called on loading the form
+    Private Sub BluetoothScanPairVBForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Commented code of Symbol.dll
+
+        'If Me.InitReader() Then
+        '    Me.StartRead()
+        'Else
+        '    Me.Close()
+        '    Return
+        'End If
+
+    End Sub
+    '' Commented code of Symbol.dll starts here
+
+    ' This method is called to intialize the symbol reader
+
+    'Private Function InitReader() As Boolean
+    '    If Me.MyReader IsNot Nothing Then
+    '        Return False
+    '    End If
+    '    Me.MyReader = New Symbol.Barcode.Reader()
+    '    Me.MyReaderData = New Symbol.Barcode.ReaderData(Symbol.Barcode.ReaderDataTypes.Text, Symbol.Barcode.ReaderDataLengths.MaximumLabel)
+    '    Me.MyEventHandler = New EventHandler(AddressOf MyReader_ReadNotify)
+    '    Me.MyReader.Actions.Enable()
+    '    Me.MyReader.Parameters.Feedback.Success.BeepTime = 0
+    '    Me.MyReader.Parameters.Feedback.Success.WaveFile = "\windows\alarm3.wav"
+    '    AddHandler Me.Activated, New EventHandler(AddressOf ReaderForm_Activated)
+    '    AddHandler Me.Deactivate, New EventHandler(AddressOf ReaderForm_Deactivate)
+    '    Return True
+    'End Function
+
+    'Private Sub StartRead()
+    '    If (Me.MyReader IsNot Nothing) AndAlso (Me.MyReaderData IsNot Nothing) Then
+    '        AddHandler Me.MyReader.ReadNotify, Me.MyEventHandler
+    '        Me.MyReader.Actions.Read(Me.MyReaderData)
+    '    End If
+    'End Sub
+
+    'Private Sub MyReader_ReadNotify(ByVal sender As Object, ByVal e As EventArgs)
+    '    Dim TheReaderData As Symbol.Barcode.ReaderData = Me.MyReader.GetNextReaderData()
+    '    If TheReaderData.Result = Symbol.Results.SUCCESS Then
+    '        Me.HandleData(TheReaderData)
+    '        Me.StartRead()
+    '    End If
+    'End Sub
+
+    'Private Sub HandleData(ByVal TheReaderData As Symbol.Barcode.ReaderData)
+    '    macAddrBox.Text = TheReaderData.Text
+    'End Sub
+
+    'Private Sub ReaderForm_Activated(ByVal sender As Object, ByVal e As EventArgs)
+    '    If Not Me.MyReaderData.IsPending Then
+    '        Me.StartRead()
+    '    End If
+    'End Sub
+
+    'Private Sub ReaderForm_Deactivate(ByVal sender As Object, ByVal e As EventArgs)
+    '    Me.StopRead()
+    'End Sub
+
+    'Private Sub StopRead()
+    '    If Me.MyReader IsNot Nothing Then
+    '        RemoveHandler Me.MyReader.ReadNotify, Me.MyEventHandler
+    '        Me.MyReader.Actions.Flush()
+    '    End If
+    'End Sub
+
+    '' Symbol code ends here
+
+    ' This method is called when Pair button is clicked. Open a bluetooth connection with printer using MAC address
+    Private Sub pairButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pairButton.Click
+        If macAddrBox.Text.Length <> 12 Then
+            MessageBox.Show("Enter valid MAC address!")
+            Return
+        End If
+
+        'Creating a bluetooth connection with zebra printer
+        If connection Is Nothing AndAlso macAddrBox.Text.Length = 12 Then
+            connection = New BluetoothPrinterConnection(macAddrBox.Text)
+            statusLabel.Text = "Status: Pairing to..." + macAddrBox.Text
+            statusLabel.BackColor = System.Drawing.Color.Gold
+            Application.DoEvents()
+            Try
+                connection.Open()
+                While Not connection.IsConnected()
+                End While
+            Catch generatedExceptionName As ZebraPrinterConnectionException
+                statusLabel.Text = "Status: Unpaired"
+                statusLabel.BackColor = System.Drawing.Color.Red
+                connection = Nothing
+                isConnected = False
+                MessageBox.Show("Unable to pair with printer.")
+                Return
+            Catch generatedExceptionName As ZebraException
+                statusLabel.BackColor = System.Drawing.Color.Red
+                statusLabel.Text = "Status : Unpaired."
+                connection = Nothing
+                isConnected = False
+                MessageBox.Show("Unable to pair with printer." & vbCr & vbLf & "Check that printer is on.")
+                Return
+            Catch generatedExceptionName As Exception
+                statusLabel.BackColor = System.Drawing.Color.Red
+                statusLabel.Text = "Status : Unpaired."
+                connection = Nothing
+                isConnected = False
+                MessageBox.Show("Pairing Failed.")
+                Return
+            End Try
+            statusLabel.Text = "Status: Paired to " + macAddrBox.Text
+            statusLabel.BackColor = System.Drawing.Color.Green
+            isConnected = True
+        End If
+    End Sub
+    ' This method is called when Unpair button is clicked. Close the Bluetooth connection with printer.
+    Private Sub unpairButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles unpairButton.Click
+        Try
+            ' Close the connection
+            If connection IsNot Nothing Then
+                connection.Close()
+            End If
+        Catch generatedExceptionName As ZebraPrinterConnectionException
+            isConnected = False
+            MessageBox.Show("Unable to unpair with printer.")
+            Return
+        Catch generatedExceptionName As ZebraException
+            isConnected = False
+            MessageBox.Show("Unable to unpair with printer." & vbCr & vbLf & "Check that printer is on.")
+            Return
+        Catch generatedExceptionName As Exception
+            isConnected = False
+            MessageBox.Show("Unpair Failed.")
+            Return
+        End Try
+        connection = Nothing
+        isConnected = False
+        statusLabel.Text = "Status: Unpaired"
+        statusLabel.BackColor = System.Drawing.Color.Red
+        sendTxtBox.Text = ""
+        sendFileBox.Text = ""
+    End Sub
+    ' This method is called when Quit button is clicked.Close the Bluetooth connection and application.
+    Private Sub quitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles quitButton.Click
+        If connection IsNot Nothing Then
+            connection.Close()
+        End If
+
+        Me.Dispose()
+    End Sub
+    ' This method is called when Send File button is clicked. Sends selected file to zebra printer.
+    Private Sub sendFileButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles sendFileButton.Click
+        If False = isConnected Then
+            MessageBox.Show("Device not paired!")
+            Return
+        End If
+
+        If sendFileBox.Text.Length = 0 Then
+            MessageBox.Show("Please Select file to print!!!")
+            Return
+        End If
+        ' Read file content, convert into bytes and send it to zebra printer.
+        If connection IsNot Nothing AndAlso sendFileBox.Text IsNot Nothing Then
+            Try
+                Dim fs As FileStream = File.OpenRead(sendFileBox.Text)
+                Dim data As Byte() = New Byte(fs.Length - 1) {}
+                fs.Read(data, 0, data.Length)
+                connection.Write(data)
+                fs.Close()
+            Catch generatedExceptionName As ZebraPrinterConnectionException
+                isConnected = False
+                connection = Nothing
+                MessageBox.Show("Unable to send file!!!" & vbCr & vbLf & "Check that printer is connected.")
+                Return
+            Catch generatedExceptionName As ZebraException
+                isConnected = False
+                connection = Nothing
+                MessageBox.Show("Unable to send file!!!." & vbCr & vbLf & "Check that printer is on.")
+                Return
+            Catch generatedExceptionName As Exception
+                isConnected = False
+                connection = Nothing
+                MessageBox.Show("Send file Failed.")
+                Return
+            End Try
+        End If
+    End Sub
+    ' This method is called when Browse button is clicked. Opens file dialog to select a file.
+    Private Sub browseButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles browseButton.Click
+        Dim ofd As New OpenFileDialog()
+        If ofd.ShowDialog() = DialogResult.OK Then
+            sendFileBox.Text = ofd.FileName
+        Else
+            sendFileBox.Text = Nothing
+        End If
+    End Sub
+    ' This method is called when Send Text button is clicked.Sends raw text to zebra printer.
+    Private Sub sendTxtButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles sendTxtButton.Click
+        Dim printString As [String] = ""
+        If False = isConnected Then
+            MessageBox.Show("Device not paired!")
+            Return
+        End If
+
+        If sendTxtBox.Text.Length = 0 Then
+            MessageBox.Show("Please Enter text to print!!!")
+            Return
+        End If
+
+        If connection IsNot Nothing AndAlso sendTxtBox.Text.Length <> 0 Then
+            Try
+                printString = "! 0 200 200 500 1" & vbCr & vbLf & "LABEL" & vbCr & vbLf & "PAGE-WIDTH 600" & vbCr & vbLf
+                printString += "T 7 0 125 158 "
+                printString += sendTxtBox.Text
+                printString += vbCr & vbLf
+                printString += "PRINT" & vbCr & vbLf
+                connection.Write(StringUtilities.GetBytes(printString))
+            Catch generatedExceptionName As ZebraPrinterConnectionException
+                isConnected = False
+                connection = Nothing
+                MessageBox.Show("Unable to send text!!!" & vbCr & vbLf & "Check that printer is connected.")
+                Return
+            Catch generatedExceptionName As ZebraException
+                isConnected = False
+                connection = Nothing
+                MessageBox.Show("Unable to send text!!!." & vbCr & vbLf & "Check that printer is on.")
+                Return
+            Catch generatedExceptionName As Exception
+                isConnected = False
+                connection = Nothing
+                MessageBox.Show("Send text Failed.")
+                Return
+            End Try
+        End If
+    End Sub
+
+    Private Sub BluetoothScanPairVBForm_Closing(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        If connection IsNot Nothing Then
+            connection.Close()
+        End If
+    End Sub
+End Class
