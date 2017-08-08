3663087ed8a1eba3987be9b6c8a7f89490fc3f40
Corn <SCorn@zebra.com>
2017-08-08 16:04:50 -0500

    Initial MagneticCardReaderExample_C# project commit

---
+++ MagneticCardReaderExample_C#/Magnetic Card Reader/MagCardDemoForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿/********************************************** 
+ * CONFIDENTIAL AND PROPRIETARY 
+ *
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * 
+ * Copyright ZIH Corp. 2010
+ *
+ * ALL RIGHTS RESERVED 
+ ***********************************************
+ File:   MagCardDemoForm.Designer.cs
+
+ Description: Layout and design for MagCardDemoForm.
+
+ $Revision: 1 $
+ $Date: 2010/06/07 $
+ *******************************************************************************/
+
+namespace Magnetic_Card_Reader
+{
+    partial class MagCardDemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.connectButton = new System.Windows.Forms.MenuItem();
+            this.readButton = new System.Windows.Forms.MenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.track1Value = new System.Windows.Forms.TextBox();
+            this.track2Value = new System.Windows.Forms.TextBox();
+            this.track3Value = new System.Windows.Forms.TextBox();
+            this.macTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusBar = new System.Windows.Forms.Label();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.connectButton);
+            this.mainMenu1.MenuItems.Add(this.readButton);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Text = "Connect";
+            this.connectButton.Click += new System.EventHandler(this.onConnectButtonClick);
+            // 
+            // readButton
+            // 
+            this.readButton.Text = "Read";
+            this.readButton.Click += new System.EventHandler(this.onReadButtonClick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.Text = "Track 1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.Text = "Track 2";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(5, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.Text = "Track 3";
+            // 
+            // track1Value
+            // 
+            this.track1Value.Location = new System.Drawing.Point(61, 163);
+            this.track1Value.Name = "track1Value";
+            this.track1Value.Size = new System.Drawing.Size(174, 21);
+            this.track1Value.TabIndex = 3;
+            // 
+            // track2Value
+            // 
+            this.track2Value.Location = new System.Drawing.Point(61, 190);
+            this.track2Value.Name = "track2Value";
+            this.track2Value.Size = new System.Drawing.Size(174, 21);
+            this.track2Value.TabIndex = 4;
+            // 
+            // track3Value
+            // 
+            this.track3Value.Location = new System.Drawing.Point(61, 217);
+            this.track3Value.Name = "track3Value";
+            this.track3Value.Size = new System.Drawing.Size(174, 21);
+            this.track3Value.TabIndex = 5;
+            // 
+            // macTextBox
+            // 
+            this.macTextBox.Location = new System.Drawing.Point(143, 24);
+            this.macTextBox.Name = "macTextBox";
+            this.macTextBox.Size = new System.Drawing.Size(92, 21);
+            this.macTextBox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(3, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 20);
+            this.label4.Text = "Bluetooth MAC Address";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Location = new System.Drawing.Point(5, 4);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(230, 20);
+            this.statusBar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(5, 245);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(105, 20);
+            this.disconnectButton.TabIndex = 13;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButtonClicked);
+            // 
+            // MagCardDemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.macTextBox);
+            this.Controls.Add(this.track3Value);
+            this.Controls.Add(this.track2Value);
+            this.Controls.Add(this.track1Value);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Menu = this.mainMenu1;
+            this.MinimizeBox = false;
+            this.Name = "MagCardDemoForm";
+            this.Text = "Mag Card Demo";
+            this.Closing += new System.ComponentModel.CancelEventHandler(this.onFrameClose);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox track1Value;
+        private System.Windows.Forms.TextBox track2Value;
+        private System.Windows.Forms.TextBox track3Value;
+        private System.Windows.Forms.MenuItem connectButton;
+        private System.Windows.Forms.MenuItem readButton;
+        private System.Windows.Forms.TextBox macTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label statusBar;
+        private System.Windows.Forms.Button disconnectButton;
+    }
+}
---
+++ MagneticCardReaderExample_C#/Magnetic Card Reader/MagCardDemoForm.cs
@@ -0,0 +1,345 @@
+﻿/********************************************** 
+ * CONFIDENTIAL AND PROPRIETARY 
+ *
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * 
+ * Copyright ZIH Corp. 2010
+ *
+ * ALL RIGHTS RESERVED 
+ ***********************************************
+ File:   MagCardDemoForm.cs
+
+ Description: Form and code demonstrating how to connect to a Zebra printer and
+ * read a magnetic card from the card reader on the printer. The printer must
+ * have a magnetic card reader in order for this application to work. 
+
+ $Revision: 1 $
+ $Date: 2010/06/07 $
+ *******************************************************************************/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using ZSDK_API.ApiException;
+using ZSDK_API.Comm;
+using ZSDK_API.Printer;
+
+namespace Magnetic_Card_Reader
+{
+    public partial class MagCardDemoForm : Form
+    {
+        String macAddress;
+        ZebraPrinterConnection connection;
+        ZebraPrinter printer;
+
+        public MagCardDemoForm()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         * Called when "Connect" button is clicked
+         * Starts Bluetooth connection
+         * **/
+        public void onConnectButtonClick(Object sender, EventArgs args)
+        {
+            UpdateTracks(new String[] { "", "", "" });
+            connectBT(macTextBox.Text.Trim());
+            updateButtonFromWorkerThread(false);
+        }
+
+        /**
+         * Called when "Read" Button is clicked
+         * Starts card reading if device is already connected to a printer
+         * **/
+        public void onReadButtonClick(Object sender, EventArgs args)
+        {
+            UpdateTracks(new String[]{"","",""});
+            updateButtonFromWorkerThread(false);
+            Thread t = new Thread(doReadMagCard);
+            t.Start();
+        }
+
+        /**
+         * When disconnectButton is clicked, spawn a thread to disconnect from the
+         * currently connected printer
+         * **/
+        public void disconnectButtonClicked(Object sender, EventArgs args)
+        {
+            disconnect();
+        }
+
+        /**
+         * Called when the frame has been closed. Closes the open connection to 
+         * a printer if one exists. 
+         * **/
+        public void onFrameClose(Object sender, CancelEventArgs args)
+        {
+            if (connection != null && connection.IsConnected())
+            {
+                connection.Close();
+                connection = null;
+            }
+        }
+
+        /**
+         * Thread for reading magnetic card swiped through printer
+         * **/
+        private void doReadMagCard() {
+            updateGuiFromWorkerThread("Waiting for swipe...", Color.Gold);
+            if (printer != null) {
+                MagCardReader mcr = printer.GetMagCardReader();
+                if (mcr != null) {
+                    String[] tracks = mcr.Read(10 * 1000);
+                    updateGuiTracks(tracks);
+                    updateGuiFromWorkerThread("Done", Color.Blue);
+                } else {
+                    updateGuiFromWorkerThread("Failed to swipe", Color.Red);
+                }
+            } else {
+                updateGuiFromWorkerThread("Connection error", Color.Red);
+            }
+            updateButtonFromWorkerThread(true);
+        }
+
+        /** 
+         * Delegate and method for updating track values from thread
+         * **/
+        private delegate void MagCardEventHandler(String[] e);
+        public void updateGuiTracks(String[] tracks) {
+            object[] pList = { tracks };
+            Invoke(new MagCardEventHandler(UpdateTracks), pList);
+        }
+        private void UpdateTracks(String[] tracks) {
+            track1Value.Text = tracks[0];
+            track2Value.Text = tracks[1];
+            track3Value.Text = tracks[2];
+        }
+
+        /***********************************************************************
+         * The following methods handle connection to printer
+         * ********************************************************************/
+
+        private void connectBT(String macAddress)
+        {
+
+            this.macAddress = macAddress;
+            Thread t = new Thread(doConnectBT);
+            t.Start();
+        }
+        /**
+        * Check that macAddress is valid
+        * Connect to device using BluetoothPrinterConnection class
+        * **/
+
+        private void doConnectBT()
+        {
+            
+            if (connection != null)
+            {
+                Thread t = disconnect();
+                t.Join();
+            }
+            updateButtonFromWorkerThread(false);
+            if (this.macAddress == null || this.macAddress.Length != 12)
+            {
+                updateGuiFromWorkerThread("Invalid MAC Address", Color.Red);
+                disconnect();
+            }
+            else
+            {
+                updateGuiFromWorkerThread("Connecting... Please wait...", Color.Goldenrod);
+                try
+                {
+                    try
+                    {
+                        connection = new BluetoothPrinterConnection(this.macAddress);
+                        threadedConnect(this.macAddress);
+                    }
+                    catch (ZebraException)
+                    {
+                        updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red);
+                        Thread.Sleep(1000);
+                        disconnect();
+                    }
+                }
+                catch (ZebraPrinterConnectionException)
+                {
+                    updateGuiFromWorkerThread("Unable to connect with printer", Color.Red);
+                    disconnect();
+                }
+                catch (ZebraException)
+                {
+                    updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red);
+                    disconnect();
+                }
+
+                catch (Exception)
+                {
+                    updateGuiFromWorkerThread("Error communicating with printer", Color.Red);
+                    disconnect();
+                }
+                updateButtonFromWorkerThread(true);
+            }
+            updateButtonFromWorkerThread(true);
+            
+        }
+        private void threadedConnect(String addressName)
+        {
+            try
+            {
+                connection.Open();
+                Thread.Sleep(1000);
+            }
+            catch (ZebraPrinterConnectionException)
+            {
+                updateGuiFromWorkerThread("Unable to connect with printer", Color.Red);
+                disconnect();
+            }
+            catch (Exception)
+            {
+                updateGuiFromWorkerThread("Error communicating with printer", Color.Red);
+                disconnect();
+            }
+
+            printer = null;
+            if (connection != null && connection.IsConnected())
+            {
+                try
+                {
+                    updateGuiFromWorkerThread("Getting printer...", Color.Goldenrod);
+                    Thread.Sleep(1000);
+                    printer = ZebraPrinterFactory.GetInstance(connection);
+                    updateGuiFromWorkerThread("Got Printer, getting Language...", Color.LemonChiffon);
+                    Thread.Sleep(1000);
+                    PrinterLanguage pl = printer.GetPrinterControlLanguage();
+                    updateGuiFromWorkerThread("Printer Language " + pl.ToString(), Color.LemonChiffon);
+                    Thread.Sleep(1000);
+                    updateGuiFromWorkerThread("Connected to " + addressName, Color.Green);
+                    Thread.Sleep(1000);
+                    //disconnect();
+                }
+                catch (ZebraPrinterConnectionException)
+                {
+                    updateGuiFromWorkerThread("Unknown Printer Language", Color.Red);
+                    printer = null;
+                    Thread.Sleep(1000);
+                    disconnect();
+                }
+                catch (ZebraPrinterLanguageUnknownException)
+                {
+                    updateGuiFromWorkerThread("Unknown Printer Language", Color.Red);
+                    printer = null;
+                    Thread.Sleep(1000);
+                    disconnect();
+                }
+            }
+            updateButtonFromWorkerThread(true);
+        }
+         /**
+         * Start disconnect thread
+         * **/
+        public Thread disconnect()
+        {
+            Thread t = new Thread(doDisconnect);
+            t.Start();
+            return t;
+        }
+
+        /**
+         * Close connection to printer
+         * **/
+        private void doDisconnect()
+        {
+            updateButtonFromWorkerThread(false);
+            try
+            {
+                if (connection != null && connection.IsConnected())
+                {
+                    updateGuiFromWorkerThread("Disconnecting...", Color.Honeydew);
+                    
+                    connection.Close();
+                }
+            }
+            catch (ZebraException)
+            {
+                updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red);
+            }
+            Thread.Sleep(1000);
+            updateGuiFromWorkerThread("Not Connected", Color.Red);
+            Thread.Sleep(1000);
+            updateButtonFromWorkerThread(true);
+            updateGuiFromWorkerThread("Ready", Color.Yellow);
+            connection = null;
+            updateButtonFromWorkerThread(true);
+        }
+        /*******************************************************************************
+         * End printer connection methods
+         * ****************************************************************************/
+
+
+        /*******************************************************************************
+         * The following methods update the status bar at top of screen
+         * ****************************************************************************/
+
+        public void updateGuiFromWorkerThread(String message, Color color)
+        {
+            Invoke(new StatusEventHandler(UpdateUI), new StatusArguments(message, color));
+        }
+
+        private delegate void StatusEventHandler(StatusArguments e);
+
+        private void UpdateUI(StatusArguments e)
+        {
+            statusBar.Text = e.message;
+            statusBar.BackColor = e.color;
+        }
+
+        public class StatusArguments : System.EventArgs
+        {
+            public String message;
+            public Color color;
+
+            public StatusArguments(String message, Color color)
+            {
+                this.message = message;
+                this.color = color;
+            }
+        }
+        /*******************************************************************************
+         * End status bar update methods
+         * ****************************************************************************/
+
+
+        /*******************************************************************************
+         * The following methods enable and disable buttons as needed
+         * ****************************************************************************/
+
+        public void updateButtonFromWorkerThread(bool enabled)
+        {
+            Invoke(new ButtonStatusHandler(UpdateButton), enabled);
+        }
+        private delegate void ButtonStatusHandler(bool enabled);
+        private void UpdateButton(bool enabled)
+        {
+            readButton.Enabled = enabled;
+            connectButton.Enabled = enabled;
+            disconnectButton.Enabled = enabled;
+        }
+        /*******************************************************************************
+         * End enable button methods
+         * ****************************************************************************/
+
+    }
+}
