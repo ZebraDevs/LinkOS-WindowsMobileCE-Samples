8add35ce1b4b246e3101d602ebbc597ff90a9d17
Corn <SCorn@zebra.com>
2017-08-08 15:33:19 -0500

    Initial TCPDiscoveryDemo_VB.NET project commit

---
+++ TCPDiscoveryDemo_VB.NET/Discovery Demo VB/DiscoveryMain.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Public Class DiscoveryMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+    Private mainMenu1 As System.Windows.Forms.MainMenu
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.mainMenu1 = New System.Windows.Forms.MainMenu
+        Me.discoveryButton = New System.Windows.Forms.MenuItem
+        Me.connectButton = New System.Windows.Forms.MenuItem
+        Me.portTextBox = New System.Windows.Forms.TextBox
+        Me.label1 = New System.Windows.Forms.Label
+        Me.topScreenPanel = New System.Windows.Forms.Panel
+        Me.userLayoutPanel = New System.Windows.Forms.Panel
+        Me.multicastPanel = New System.Windows.Forms.Panel
+        Me.multicastHopUpDown = New System.Windows.Forms.NumericUpDown
+        Me.multicastHopsLabel = New System.Windows.Forms.Label
+        Me.directBroadcastPanel = New System.Windows.Forms.Panel
+        Me.directBroadcastTextBox = New System.Windows.Forms.TextBox
+        Me.directLabel = New System.Windows.Forms.Label
+        Me.subnetPanel = New System.Windows.Forms.Panel
+        Me.subnetTextBox = New System.Windows.Forms.TextBox
+        Me.subnetLabel = New System.Windows.Forms.Label
+        Me.discoveredPrintersLabel = New System.Windows.Forms.Label
+        Me.methodLabel = New System.Windows.Forms.Label
+        Me.discoveryMethodsComboBox = New System.Windows.Forms.ComboBox
+        Me.statusBar = New System.Windows.Forms.Label
+        Me.discoveredPrintersListBox = New System.Windows.Forms.ListBox
+        Me.topScreenPanel.SuspendLayout()
+        Me.userLayoutPanel.SuspendLayout()
+        Me.multicastPanel.SuspendLayout()
+        Me.directBroadcastPanel.SuspendLayout()
+        Me.subnetPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'mainMenu1
+        '
+        Me.mainMenu1.MenuItems.Add(Me.discoveryButton)
+        Me.mainMenu1.MenuItems.Add(Me.connectButton)
+        '
+        'discoveryButton
+        '
+        Me.discoveryButton.Text = "Discovery"
+        '
+        'connectButton
+        '
+        Me.connectButton.Text = "Connect"
+        '
+        'portTextBox
+        '
+        Me.portTextBox.Location = New System.Drawing.Point(188, 244)
+        Me.portTextBox.Name = "portTextBox"
+        Me.portTextBox.Size = New System.Drawing.Size(51, 21)
+        Me.portTextBox.TabIndex = 17
+        Me.portTextBox.Text = "6101"
+        '
+        'label1
+        '
+        Me.label1.Location = New System.Drawing.Point(150, 244)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(32, 20)
+        Me.label1.Text = "Port:"
+        '
+        'topScreenPanel
+        '
+        Me.topScreenPanel.BackColor = System.Drawing.Color.DarkGray
+        Me.topScreenPanel.Controls.Add(Me.userLayoutPanel)
+        Me.topScreenPanel.Controls.Add(Me.statusBar)
+        Me.topScreenPanel.Location = New System.Drawing.Point(0, 1)
+        Me.topScreenPanel.Name = "topScreenPanel"
+        Me.topScreenPanel.Size = New System.Drawing.Size(240, 127)
+        '
+        'userLayoutPanel
+        '
+        Me.userLayoutPanel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.userLayoutPanel.BackColor = System.Drawing.Color.DarkGray
+        Me.userLayoutPanel.Controls.Add(Me.multicastPanel)
+        Me.userLayoutPanel.Controls.Add(Me.directBroadcastPanel)
+        Me.userLayoutPanel.Controls.Add(Me.subnetPanel)
+        Me.userLayoutPanel.Controls.Add(Me.discoveredPrintersLabel)
+        Me.userLayoutPanel.Controls.Add(Me.methodLabel)
+        Me.userLayoutPanel.Controls.Add(Me.discoveryMethodsComboBox)
+        Me.userLayoutPanel.Location = New System.Drawing.Point(9, 23)
+        Me.userLayoutPanel.Name = "userLayoutPanel"
+        Me.userLayoutPanel.Size = New System.Drawing.Size(228, 101)
+        '
+        'multicastPanel
+        '
+        Me.multicastPanel.BackColor = System.Drawing.Color.DarkGray
+        Me.multicastPanel.Controls.Add(Me.multicastHopUpDown)
+        Me.multicastPanel.Controls.Add(Me.multicastHopsLabel)
+        Me.multicastPanel.Location = New System.Drawing.Point(1, 31)
+        Me.multicastPanel.Name = "multicastPanel"
+        Me.multicastPanel.Size = New System.Drawing.Size(196, 32)
+        '
+        'multicastHopUpDown
+        '
+        Me.multicastHopUpDown.Location = New System.Drawing.Point(98, 2)
+        Me.multicastHopUpDown.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.multicastHopUpDown.Name = "multicastHopUpDown"
+        Me.multicastHopUpDown.Size = New System.Drawing.Size(53, 22)
+        Me.multicastHopUpDown.TabIndex = 6
+        Me.multicastHopUpDown.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        '
+        'multicastHopsLabel
+        '
+        Me.multicastHopsLabel.Location = New System.Drawing.Point(3, 4)
+        Me.multicastHopsLabel.Name = "multicastHopsLabel"
+        Me.multicastHopsLabel.Size = New System.Drawing.Size(89, 20)
+        Me.multicastHopsLabel.Text = "Multicast Hops:"
+        '
+        'directBroadcastPanel
+        '
+        Me.directBroadcastPanel.BackColor = System.Drawing.Color.DarkGray
+        Me.directBroadcastPanel.Controls.Add(Me.directBroadcastTextBox)
+        Me.directBroadcastPanel.Controls.Add(Me.directLabel)
+        Me.directBroadcastPanel.Location = New System.Drawing.Point(20, 30)
+        Me.directBroadcastPanel.Name = "directBroadcastPanel"
+        Me.directBroadcastPanel.Size = New System.Drawing.Size(177, 33)
+        Me.directBroadcastPanel.Visible = False
+        '
+        'directBroadcastTextBox
+        '
+        Me.directBroadcastTextBox.Location = New System.Drawing.Point(55, 3)
+        Me.directBroadcastTextBox.Name = "directBroadcastTextBox"
+        Me.directBroadcastTextBox.Size = New System.Drawing.Size(111, 21)
+        Me.directBroadcastTextBox.TabIndex = 8
+        Me.directBroadcastTextBox.Text = ".255"
+        '
+        'directLabel
+        '
+        Me.directLabel.Location = New System.Drawing.Point(3, 3)
+        Me.directLabel.Name = "directLabel"
+        Me.directLabel.Size = New System.Drawing.Size(46, 20)
+        Me.directLabel.Text = "Range: "
+        '
+        'subnetPanel
+        '
+        Me.subnetPanel.BackColor = System.Drawing.Color.DarkGray
+        Me.subnetPanel.Controls.Add(Me.subnetTextBox)
+        Me.subnetPanel.Controls.Add(Me.subnetLabel)
+        Me.subnetPanel.Location = New System.Drawing.Point(20, 31)
+        Me.subnetPanel.Name = "subnetPanel"
+        Me.subnetPanel.Size = New System.Drawing.Size(173, 32)
+        Me.subnetPanel.Visible = False
+        '
+        'subnetTextBox
+        '
+        Me.subnetTextBox.Location = New System.Drawing.Point(55, 0)
+        Me.subnetTextBox.Name = "subnetTextBox"
+        Me.subnetTextBox.Size = New System.Drawing.Size(111, 21)
+        Me.subnetTextBox.TabIndex = 8
+        Me.subnetTextBox.Text = ".*"
+        '
+        'subnetLabel
+        '
+        Me.subnetLabel.Location = New System.Drawing.Point(3, 3)
+        Me.subnetLabel.Name = "subnetLabel"
+        Me.subnetLabel.Size = New System.Drawing.Size(46, 20)
+        Me.subnetLabel.Text = "Subnet:"
+        '
+        'discoveredPrintersLabel
+        '
+        Me.discoveredPrintersLabel.Location = New System.Drawing.Point(22, 73)
+        Me.discoveredPrintersLabel.Name = "discoveredPrintersLabel"
+        Me.discoveredPrintersLabel.Size = New System.Drawing.Size(155, 14)
+        Me.discoveredPrintersLabel.Text = "Discovered Printers"
+        Me.discoveredPrintersLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'methodLabel
+        '
+        Me.methodLabel.Location = New System.Drawing.Point(1, 7)
+        Me.methodLabel.Name = "methodLabel"
+        Me.methodLabel.Size = New System.Drawing.Size(54, 20)
+        Me.methodLabel.Text = "Method:"
+        '
+        'discoveryMethodsComboBox
+        '
+        Me.discoveryMethodsComboBox.Items.Add("Multicast")
+        Me.discoveryMethodsComboBox.Items.Add("Directed Broadcast")
+        Me.discoveryMethodsComboBox.Items.Add("Local Broadcast")
+        Me.discoveryMethodsComboBox.Items.Add("Subnet Search")
+        Me.discoveryMethodsComboBox.Location = New System.Drawing.Point(61, 3)
+        Me.discoveryMethodsComboBox.Name = "discoveryMethodsComboBox"
+        Me.discoveryMethodsComboBox.Size = New System.Drawing.Size(129, 22)
+        Me.discoveryMethodsComboBox.TabIndex = 12
+        '
+        'statusBar
+        '
+        Me.statusBar.BackColor = System.Drawing.Color.Red
+        Me.statusBar.Dock = System.Windows.Forms.DockStyle.Top
+        Me.statusBar.Location = New System.Drawing.Point(0, 0)
+        Me.statusBar.Name = "statusBar"
+        Me.statusBar.Size = New System.Drawing.Size(240, 20)
+        Me.statusBar.Text = "Status: Not Connected"
+        Me.statusBar.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'discoveredPrintersListBox
+        '
+        Me.discoveredPrintersListBox.BackColor = System.Drawing.Color.LightGray
+        Me.discoveredPrintersListBox.Location = New System.Drawing.Point(0, 127)
+        Me.discoveredPrintersListBox.Name = "discoveredPrintersListBox"
+        Me.discoveredPrintersListBox.Size = New System.Drawing.Size(240, 114)
+        Me.discoveredPrintersListBox.TabIndex = 20
+        '
+        'DiscoveryMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(240, 268)
+        Me.Controls.Add(Me.topScreenPanel)
+        Me.Controls.Add(Me.discoveredPrintersListBox)
+        Me.Controls.Add(Me.portTextBox)
+        Me.Controls.Add(Me.label1)
+        Me.Menu = Me.mainMenu1
+        Me.MinimizeBox = False
+        Me.Name = "DiscoveryMain"
+        Me.Text = "Discovery Demo VB"
+        Me.topScreenPanel.ResumeLayout(False)
+        Me.userLayoutPanel.ResumeLayout(False)
+        Me.multicastPanel.ResumeLayout(False)
+        Me.directBroadcastPanel.ResumeLayout(False)
+        Me.subnetPanel.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents discoveryButton As System.Windows.Forms.MenuItem
+    Friend WithEvents connectButton As System.Windows.Forms.MenuItem
+    Private WithEvents portTextBox As System.Windows.Forms.TextBox
+    Private WithEvents label1 As System.Windows.Forms.Label
+    Private WithEvents topScreenPanel As System.Windows.Forms.Panel
+    Private WithEvents userLayoutPanel As System.Windows.Forms.Panel
+    Private WithEvents multicastPanel As System.Windows.Forms.Panel
+    Private WithEvents multicastHopUpDown As System.Windows.Forms.NumericUpDown
+    Private WithEvents multicastHopsLabel As System.Windows.Forms.Label
+    Private WithEvents directBroadcastPanel As System.Windows.Forms.Panel
+    Private WithEvents directBroadcastTextBox As System.Windows.Forms.TextBox
+    Private WithEvents directLabel As System.Windows.Forms.Label
+    Private WithEvents subnetPanel As System.Windows.Forms.Panel
+    Private WithEvents subnetTextBox As System.Windows.Forms.TextBox
+    Private WithEvents subnetLabel As System.Windows.Forms.Label
+    Private WithEvents discoveredPrintersLabel As System.Windows.Forms.Label
+    Private WithEvents methodLabel As System.Windows.Forms.Label
+    Private WithEvents discoveryMethodsComboBox As System.Windows.Forms.ComboBox
+    Private WithEvents statusBar As System.Windows.Forms.Label
+    Private WithEvents discoveredPrintersListBox As System.Windows.Forms.ListBox
+
+End Class
---
+++ TCPDiscoveryDemo_VB.NET/Discovery Demo VB/DiscoveryMain.vb
@@ -0,0 +1,306 @@
+﻿' CONFIDENTIAL AND PROPRIETARY 
+'
+' The source code and other information contained herein is the confidential and the exclusive property of
+' ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+' This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+' displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+' expressly permitted under such license agreement.
+'
+' Copyright ZIH Corp. 2010
+'
+' ALL RIGHTS RESERVED 
+'
+' File:   DiscoveryMain.vb
+'
+' Description: Form and code demonstrating how to discover and connect to printers
+' located on the network
+'
+'$Revision: 1 $
+'$Date: 2011/04/01 $
+
+Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports ZSDK_API.Discovery
+Imports System.Threading
+Imports ZSDK_API.ApiException
+Imports ZSDK_API.Comm
+Imports ZSDK_API.Printer
+Imports System.Text
+
+Public Class DiscoveryMain
+    Private ipAddress As String = "127.0.0.1"
+    Private portNumber As Integer = 6101
+    Private connection As ZebraPrinterConnection
+    Private printer As ZebraPrinter
+    Private discoveryMethodIndex As Integer
+    Private multicastValue As Integer
+    Private directedBroadcastAddress As String
+    Private subnetAddress As String
+
+    Public Sub New()
+        ' This call is required by the Windows Form Designer.
+        InitializeComponent()
+        discoveryMethodsComboBox.SelectedIndex = discoveryMethodsComboBox.Items.IndexOf("Multicast")
+    End Sub
+
+    Private Sub discoveryMethodsComboBox_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles discoveryMethodsComboBox.SelectedIndexChanged
+        Dim methodSelection As Integer = discoveryMethodsComboBox.SelectedIndex
+        Select Case (methodSelection)
+            Case 0
+                showMulticastPanel()
+            Case 1
+                showDirectBroadcastPanel()
+            Case 3
+                showSubnetPanel()
+            Case Else
+                hidePanels()
+        End Select
+    End Sub
+
+    Private Sub showMulticastPanel()
+        directBroadcastPanel.Visible = False
+        subnetPanel.Visible = False
+        multicastPanel.Visible = True
+    End Sub
+
+    Private Sub showDirectBroadcastPanel()
+        directBroadcastPanel.Visible = True
+        subnetPanel.Visible = False
+        multicastPanel.Visible = False
+    End Sub
+
+    Private Sub showSubnetPanel()
+        directBroadcastPanel.Visible = False
+        subnetPanel.Visible = True
+        multicastPanel.Visible = False
+    End Sub
+
+    Private Sub hidePanels()
+        directBroadcastPanel.Visible = False
+        subnetPanel.Visible = False
+        multicastPanel.Visible = False
+    End Sub
+
+    Private Sub discoveryButton_Click(ByVal sender As Object, ByVal e As EventArgs) Handles discoveryButton.Click
+        updateButtonFromWorkerThread(False)
+        updateGuiFromWorkerThread("Discovery in progress...", Color.HotPink)
+        discoveredPrintersListBox.Items.Clear()
+        Me.discoveryMethodIndex = discoveryMethodsComboBox.SelectedIndex
+        Me.multicastValue = Convert.ToInt32(multicastHopUpDown.Value)
+        Me.directedBroadcastAddress = directBroadcastTextBox.Text
+        Me.subnetAddress = subnetTextBox.Text
+        Dim t As New Thread(AddressOf doDiscovery)
+        t.Start()
+    End Sub
+
+    Private Sub doDiscovery()
+        Select Case (Me.discoveryMethodIndex)
+            Case 0
+                doMulticast(Me.multicastValue)
+            Case 1
+                doDirectBroadcast(Me.directedBroadcastAddress)
+            Case 2
+                doLocalBroadcast()
+            Case Else
+                doSubnetSearch(Me.subnetAddress)
+        End Select
+    End Sub
+
+    Private Sub doMulticast(ByVal hops As Integer)
+        Try
+            Dim printers() As DiscoveredPrinter = NetworkDiscoverer.Multicast(hops)
+            displayPrinters(printers)
+        Catch ex As DiscoveryException
+            handleException(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub doDirectBroadcast(ByVal directBroadcastRange As String)
+        Try
+            Dim printers() As DiscoveredPrinter = NetworkDiscoverer.DirectedBroadcast(directBroadcastRange)
+            displayPrinters(printers)
+        Catch ex As DiscoveryException
+            handleException(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub doLocalBroadcast()
+        Try
+            Dim printers() As DiscoveredPrinter = NetworkDiscoverer.LocalBroadcast
+            displayPrinters(printers)
+        Catch ex As DiscoveryException
+            handleException(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub doSubnetSearch(ByVal subnetRange As String)
+        Try
+            Dim printers() As DiscoveredPrinter = NetworkDiscoverer.SubnetSearch(subnetRange)
+            displayPrinters(printers)
+        Catch ex As DiscoveryException
+            handleException(ex.Message)
+        End Try
+    End Sub
+    Public Sub sendLabel()
+        Dim printer As ZebraPrinter = getPrinter()
+        If (Not (printer) Is Nothing) Then
+            Dim configLabel() As Byte = getConfigLabel(printer.GetPrinterControlLanguage)
+            getConnection.Write(configLabel)
+            updateGuiFromWorkerThread("Sending Data", Color.CornflowerBlue)
+            Thread.Sleep(2000)
+        End If
+        disconnect()
+    End Sub
+
+    Private Function getConfigLabel(ByVal printerLanguage As PrinterLanguage) As Byte()
+        Dim configLabel As String = ""
+        If (printerLanguage = printerLanguage.ZPL) Then
+            configLabel = "^XA^FO17,16^GB379,371,8^FS^FT65,255^A0N,135,134^FDTEST^FS^XZ"
+        ElseIf (printerLanguage = printerLanguage.CPCL) Then
+            configLabel = ("! 0 200 200 406 1" & vbCrLf + ("ON-FEED IGNORE" & vbCrLf + ("BOX 20 20 380 380 8" & vbCrLf + ("T 0 6 137 177 TEST" & vbCrLf + "PRINT" & vbCrLf))))
+        End If
+        Return Encoding.Default.GetBytes(configLabel)
+    End Function
+
+    Private Sub doConnectTcp()
+        updateGuiFromWorkerThread("Connecting... Please wait...", Color.Goldenrod)
+        Try
+            connection = New TcpPrinterConnection(Me.ipAddress, Me.portNumber)
+            threadedConnect(Me.ipAddress)
+            sendLabel()
+            disconnect()
+        Catch generatedExceptionName As ZebraException
+            updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red)
+            doDisconnect()
+        End Try
+    End Sub
+
+    Private Sub threadedConnect(ByVal addressName As String)
+        Try
+            connection.Open()
+            Thread.Sleep(1000)
+        Catch generatedExceptionName As ZebraPrinterConnectionException
+            updateGuiFromWorkerThread("Unable to connect with printer", Color.Red)
+            disconnect()
+        Catch generatedExceptionName As Exception
+            updateGuiFromWorkerThread("Error communicating with printer", Color.Red)
+            disconnect()
+        End Try
+        printer = Nothing
+        If connection IsNot Nothing AndAlso connection.IsConnected() Then
+            Try
+                printer = ZebraPrinterFactory.GetInstance(connection)
+                Dim pl As PrinterLanguage = printer.GetPrinterControlLanguage
+                updateGuiFromWorkerThread(("Printer Language " + pl.ToString), Color.LemonChiffon)
+                Thread.Sleep(1000)
+                updateGuiFromWorkerThread(("Connected to " + addressName), Color.Green)
+                Thread.Sleep(1000)
+            Catch generatedExceptionName As ZebraPrinterConnectionException
+                updateGuiFromWorkerThread("Unknown Printer Language", Color.Red)
+                printer = Nothing
+                Thread.Sleep(1000)
+                disconnect()
+            Catch generatedExceptionName As ZebraPrinterLanguageUnknownException
+                updateGuiFromWorkerThread("Unknown Printer Language", Color.Red)
+                printer = Nothing
+                Thread.Sleep(1000)
+                disconnect()
+            End Try
+        End If
+    End Sub
+
+    Public Sub disconnect()
+        Dim t As New Thread(AddressOf doDisconnect)
+        t.Start()
+    End Sub
+
+    Private Sub doDisconnect()
+        Try
+            If connection IsNot Nothing AndAlso connection.IsConnected() Then
+                updateGuiFromWorkerThread("Disconnecting...", Color.Honeydew)
+
+                connection.Close()
+            End If
+        Catch generatedExceptionName As ZebraException
+            updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red)
+        End Try
+        Thread.Sleep(1000)
+        updateGuiFromWorkerThread("Not Connected", Color.Red)
+        connection = Nothing
+        updateButtonFromWorkerThread(True)
+    End Sub
+
+    Public Function getPrinter() As ZebraPrinter
+        Return printer
+    End Function
+
+    Public Function getConnection() As ZebraPrinterConnection
+        Return connection
+    End Function
+
+    Private Sub connectButton_Click(ByVal sender As Object, ByVal e As EventArgs) Handles connectButton.Click
+        updateButtonFromWorkerThread(False)
+        ipAddress = CType(discoveredPrintersListBox.SelectedItem, String)
+        If ((Not (portTextBox.Text) Is Nothing) _
+                    AndAlso (portTextBox.Text <> "")) Then
+            portNumber = Convert.ToInt32(portTextBox.Text)
+        End If
+        Dim t As New Thread(AddressOf doConnectTcp)
+        t.Start()
+    End Sub
+    ' The following methods update the discovered printers listbox
+    Private Sub handleException(ByVal message As [String])
+        updateGuiFromWorkerThread(message, Color.Red)
+        BeginInvoke(New MyProgressEventsHandler(AddressOf UpdateDiscoveredPrinters), New Object() {Me, New DiscoveredPrinter() {}})
+    End Sub
+    Private Delegate Sub MyProgressEventsHandler(ByVal sender As Object, ByVal printers As DiscoveredPrinter())
+    Private Sub UpdateDiscoveredPrinters(ByVal sender As Object, ByVal printers As DiscoveredPrinter())
+        For Each printer As DiscoveredPrinter In printers
+            discoveredPrintersListBox.Items.Add(printer.Address)
+        Next
+        updateButtonFromWorkerThread(True)
+    End Sub
+    Private Sub displayPrinters(ByVal printers As DiscoveredPrinter())
+        updateGuiFromWorkerThread("Discovered " & printers.Length & " printers", Color.Green)
+        BeginInvoke(New MyProgressEventsHandler(AddressOf UpdateDiscoveredPrinters), New Object() {Me, printers})
+    End Sub
+    ' The following methods update the status bar at top of screen
+    Public Sub updateGuiFromWorkerThread(ByVal message As [String], ByVal color As Color)
+        Invoke(New StatusEventHandler(AddressOf UpdateUI), New StatusArguments(message, color))
+    End Sub
+    'Delegate for the status event handler
+    Private Delegate Sub StatusEventHandler(ByVal e As StatusArguments)
+    ' The following method updates the status bar
+    Private Sub UpdateUI(ByVal e As StatusArguments)
+        statusBar.Text = e.message
+        statusBar.BackColor = e.color
+    End Sub
+    ' Status Bar data class - holds the text to be displayed and the background color of the label
+    Private Class StatusArguments
+        Inherits System.EventArgs
+        Public message As [String]
+        Public color As Color
+        Public Sub New(ByVal message As [String], ByVal color As Color)
+            Me.message = message
+            Me.color = color
+        End Sub
+    End Class
+    'The following methods enable and disable buttons as needed
+    Public Sub updateButtonFromWorkerThread(ByVal enabled As Boolean)
+        Invoke(New ButtonStatusHandler(AddressOf UpdateButton), enabled)
+    End Sub
+    ' Delegate for the Button status handler
+    Private Delegate Sub ButtonStatusHandler(ByVal enabled As Boolean)
+    'The following method sets the state of the buttons on the form
+    Private Sub UpdateButton(ByVal enabled As Boolean)
+        discoveryButton.Enabled = enabled
+        connectButton.Enabled = enabled
+    End Sub
+    ' The following method is called before the form is being closed
+    Private Sub DiscoveryDemoMain_closing(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        If connection IsNot Nothing Then
+            connection.Close()
+        End If
+    End Sub
+End Class
