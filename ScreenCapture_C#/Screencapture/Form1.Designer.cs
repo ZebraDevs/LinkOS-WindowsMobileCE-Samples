b832acee25859ef5b5fd9a5f30f7688d6ceb4dcb
Corn <SCorn@zebra.com>
2017-08-08 14:40:35 -0500

    Initial ScreenCapture_C# project commit

---
+++ ScreenCapture_C#/Screencapture/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Screencapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Location = new System.Drawing.Point(75, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(21, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 55);
+            this.label1.Text = "Press the START button. Then you\'ll have 10 seconds to browse to the screen you w" +
+                "ish to print.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(21, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 20);
+            this.label2.Text = "Enter Bluetooth MAC address:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(240, 319);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Screen Capture Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ScreenCapture_C#/Screencapture/Form1.cs
@@ -0,0 +1,125 @@
+﻿/********************************************** 
+ * CONFIDENTIAL AND PROPRIETARY 
+ *
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * 
+ * Copyright ZIH Corp. 2012
+ *
+ * ALL RIGHTS RESERVED 
+ ***********************************************/
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZSDK_API.Comm;
+using ZSDK_API.ApiException;
+using ZSDK_API.Printer;
+
+
+
+
+namespace Screencapture
+{
+    
+    public partial class Form1 : Form
+    {
+        Bitmap screen_image;
+
+        enum RasterOperation : uint
+        {
+            SRC_COPY = 0x00CC0020
+        }
+
+        [DllImport("coredll.dll")]
+        static extern int BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hdcSrc, int nXSrc, int nYSrc, RasterOperation rasterOperation);
+
+        [DllImport("coredll.dll")]
+        private static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("coredll.dll")]
+        private static extern int GetDeviceCaps(IntPtr hdc, DeviceCapsIndex nIndex);
+
+        enum DeviceCapsIndex : int
+        {
+            HORZRES = 8,
+            VERTRES = 10,
+        }
+
+        public static Size PixelDimensions
+        {
+            get
+            {
+                IntPtr hdc = GetDC(IntPtr.Zero);
+                return new Size(GetDeviceCaps(hdc, DeviceCapsIndex.HORZRES), GetDeviceCaps(hdc, DeviceCapsIndex.VERTRES));
+            }
+        }
+
+        public static Bitmap GetScreenCapture()
+        {
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            Size size = PixelDimensions;
+            Bitmap bitmap = new Bitmap(size.Width, size.Height, PixelFormat.Format16bppRgb565);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                IntPtr dstHdc = graphics.GetHdc();
+                BitBlt(dstHdc, 0, 0, size.Width, size.Height, hdc, 0, 0, RasterOperation.SRC_COPY);
+                graphics.ReleaseHdc(dstHdc);
+            }
+            return bitmap;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            System.Threading.Thread.Sleep(10000);
+            try
+            {
+                screen_image = GetScreenCapture();  //Get bitmap image of the screen 
+                String myMacAddress = textBox1.Text; //Printer's Bluetooth MAC Address.
+                ZebraPrinterConnection zebraPrinterConnection = new BluetoothPrinterConnection(myMacAddress);
+                zebraPrinterConnection.Open();
+                ZebraPrinter printer = ZebraPrinterFactory.GetInstance(zebraPrinterConnection);
+
+                int x = 10;
+                int y = 200;
+                zebraPrinterConnection.Write(Encoding.Default.GetBytes("! 0 200 200 1200 1\r\n"));
+                printer.GetGraphicsUtil().PrintImage(screen_image, x, y, -1, -1, true);  //Print the screen caputre image.
+                zebraPrinterConnection.Write(Encoding.Default.GetBytes("FORM\r\nPRINT\r\n"));
+                zebraPrinterConnection.Close();
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Unable to print screen image");
+            }
+
+            this.Show();
+        }
+
+    }
+}
