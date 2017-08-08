d9ba612584dde59e33159b5f937e752d8562ed8d
Corn <SCorn@zebra.com>
2017-08-08 16:23:42 -0500

    Initial ImageRotateDemo_C# project commit

---
+++ ImageRotateDemo_C#/WinSDKImageRotate/ImageRotate.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WinSDKImageRotate
+{
+    partial class ImageRotate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageRotate));
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.PrintButton = new System.Windows.Forms.MenuItem();
+            this.radioButtonNormal = new System.Windows.Forms.RadioButton();
+            this.radioButton90 = new System.Windows.Forms.RadioButton();
+            this.radioButton180 = new System.Windows.Forms.RadioButton();
+            this.radioButton270 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.macAddressTextBox = new System.Windows.Forms.TextBox();
+            this.statusBar = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbxFolder = new System.Windows.Forms.PictureBox();
+            this.ImagePathtextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.PrintButton);
+            // 
+            // PrintButton
+            // 
+            this.PrintButton.Text = "Print";
+            this.PrintButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // radioButtonNormal
+            // 
+            this.radioButtonNormal.Checked = true;
+            this.radioButtonNormal.Location = new System.Drawing.Point(56, 155);
+            this.radioButtonNormal.Name = "radioButtonNormal";
+            this.radioButtonNormal.Size = new System.Drawing.Size(100, 20);
+            this.radioButtonNormal.TabIndex = 1;
+            this.radioButtonNormal.Text = "0 degrees";
+            // 
+            // radioButton90
+            // 
+            this.radioButton90.Location = new System.Drawing.Point(56, 172);
+            this.radioButton90.Name = "radioButton90";
+            this.radioButton90.Size = new System.Drawing.Size(100, 20);
+            this.radioButton90.TabIndex = 2;
+            this.radioButton90.Text = "90 degrees";
+            // 
+            // radioButton180
+            // 
+            this.radioButton180.Location = new System.Drawing.Point(56, 189);
+            this.radioButton180.Name = "radioButton180";
+            this.radioButton180.Size = new System.Drawing.Size(100, 20);
+            this.radioButton180.TabIndex = 3;
+            this.radioButton180.Text = "180 degrees";
+            // 
+            // radioButton270
+            // 
+            this.radioButton270.Location = new System.Drawing.Point(56, 206);
+            this.radioButton270.Name = "radioButton270";
+            this.radioButton270.Size = new System.Drawing.Size(100, 20);
+            this.radioButton270.TabIndex = 4;
+            this.radioButton270.Text = "270 degrees";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(4, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 20);
+            this.label1.Text = "Enter Printer Bluetooth MAC Address:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(4, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 14);
+            this.label2.Text = "Rotation:";
+            // 
+            // macAddressTextBox
+            // 
+            this.macAddressTextBox.Location = new System.Drawing.Point(4, 101);
+            this.macAddressTextBox.Name = "macAddressTextBox";
+            this.macAddressTextBox.Size = new System.Drawing.Size(177, 21);
+            this.macAddressTextBox.TabIndex = 13;
+            this.macAddressTextBox.Text = "00037A6D5177";
+            // 
+            // statusBar
+            // 
+            this.statusBar.BackColor = System.Drawing.Color.Red;
+            this.statusBar.Location = new System.Drawing.Point(0, 0);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(237, 20);
+            this.statusBar.Text = "Not Connected";
+            this.statusBar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(5, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(219, 20);
+            this.label3.Text = "Select / Enter file to send to printer";
+            // 
+            // pbxFolder
+            // 
+            this.pbxFolder.BackColor = System.Drawing.Color.Silver;
+            this.pbxFolder.Image = ((System.Drawing.Image)(resources.GetObject("pbxFolder.Image")));
+            this.pbxFolder.Location = new System.Drawing.Point(202, 51);
+            this.pbxFolder.Name = "pbxFolder";
+            this.pbxFolder.Size = new System.Drawing.Size(30, 30);
+            this.pbxFolder.Click += new System.EventHandler(this.pbxFolder_Click);
+            // 
+            // ImagePathtextBox
+            // 
+            this.ImagePathtextBox.Location = new System.Drawing.Point(5, 51);
+            this.ImagePathtextBox.Name = "ImagePathtextBox";
+            this.ImagePathtextBox.Size = new System.Drawing.Size(196, 21);
+            this.ImagePathtextBox.TabIndex = 16;
+            // 
+            // ImageRotate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.ImagePathtextBox);
+            this.Controls.Add(this.pbxFolder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.macAddressTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton270);
+            this.Controls.Add(this.radioButton180);
+            this.Controls.Add(this.radioButton90);
+            this.Controls.Add(this.radioButtonNormal);
+            this.Menu = this.mainMenu1;
+            this.Name = "ImageRotate";
+            this.Text = "Image Rotation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonNormal;
+        private System.Windows.Forms.RadioButton radioButton90;
+        private System.Windows.Forms.RadioButton radioButton180;
+        private System.Windows.Forms.RadioButton radioButton270;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox macAddressTextBox;
+        private System.Windows.Forms.Label statusBar;
+        private System.Windows.Forms.MenuItem PrintButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pbxFolder;
+        private System.Windows.Forms.TextBox ImagePathtextBox;
+
+    }
+}
+
---
+++ ImageRotateDemo_C#/WinSDKImageRotate/ImageRotate.cs
@@ -0,0 +1,440 @@
+﻿/********************************************** 
+ * CONFIDENTIAL AND PROPRIETARY 
+ *
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * 
+ * Copyright ZIH Corp. 2010
+ *
+ * ALL RIGHTS RESERVED 
+ ***********************************************
+ File:   ImageRotate.cs
+
+ Descr:  This file defines the functions of the GUI components.  
+         It allows for the rotation of a user selected image by calling
+         the Rotator class defined in the Rotator.cs file.  It then prints
+         the rotated image to a Zebra CPCL printer by using the ZSDK_API.dll 
+         to connect and send data to the printer via Bluetooth.
+
+ $Revision: 4.0 $
+ $Date: 06/17/2010 $
+ * 
+ * Created by Zebra Development Services Group
+***********************************************/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZSDK_API.ApiException;
+using ZSDK_API.Comm;
+using ZSDK_API.Printer;
+
+namespace WinSDKImageRotate
+{
+    public partial class ImageRotate : Form
+    {
+        private string _sFileName = "";
+        /// <summary>
+        /// Name (and path) of file to send to the printer.
+        /// </summary>
+        public string sFileName
+        {
+            get { return _sFileName; }
+            set
+            {
+                _sFileName = value;
+                ImagePathtextBox.Text = _sFileName;
+            }
+        }
+
+        String macAddress;
+        ZebraPrinterConnection connection;
+        ZebraPrinter printer;
+        Bitmap image;
+
+        public ImageRotate()
+        {
+            InitializeComponent();
+            
+        }    
+
+        /**
+         * When the "Print" button is clicked, get the image, connect and print, then disconnect
+         * **/
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            if (ImagePathtextBox.Text != "")
+            {
+                GetImage();
+                connectBT(macAddressTextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please select or enter the path of an image file to print.");
+            }
+            
+        }
+
+         /***********************************************************************
+         * The following methods handle connection to printer
+         * ********************************************************************/
+
+        /**
+         * Spawn a new thread that connects to the printer
+         * **/
+        private void connectBT(String macAddress)
+        {
+
+            this.macAddress = macAddress;
+            Thread t = new Thread(doConnectBT);
+            t.Start();
+        }
+
+
+        /**
+        * Check that macAddress is valid
+        * Connect to device using BluetoothPrinterConnection class
+        * **/
+        private void doConnectBT()
+        {
+            updateButtonFromWorkerThread(false);
+            /**
+             * If a connection already exists, disconnect it
+             * **/
+            if (connection != null)
+            {
+                Thread t = disconnect();
+                t.Join();
+            }
+            if (this.macAddress == null || this.macAddress.Length != 12)
+            {
+                updateGuiFromWorkerThread("Invalid MAC Address", Color.Red);
+                disconnect();
+            }
+            else
+            {
+                /** 
+                 * Attempt to connect to printer via Bluetooth
+                 * **/
+                updateGuiFromWorkerThread("Connecting... Please wait...", Color.Goldenrod);
+                try
+                {
+                     connection = new BluetoothPrinterConnection(this.macAddress);
+                     threadedConnect(this.macAddress);
+                }
+                catch (ZebraPrinterConnectionException)
+                {
+                    updateGuiFromWorkerThread("Unable to connect with printer", Color.Red);
+                    disconnect();
+                }
+                catch (ZebraException)
+                {
+                    updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red);
+                    disconnect();
+                }
+
+                catch (Exception)
+                {
+                    updateGuiFromWorkerThread("Error communicating with printer", Color.Red);
+                    disconnect();
+                }
+                updateButtonFromWorkerThread(true);
+            }
+            
+        }
+
+        /**
+         * When a ZebraPrinterConnection has been established, this method 
+         * gets the printer and gathers information from the printer such as 
+         * the printer language and prints to the printer
+         * **/
+
+        private void threadedConnect(String addressName)
+        {
+            try
+            {
+                /**
+                 * Open the connection
+                 * **/
+                connection.Open();
+                Thread.Sleep(1000);
+            }
+            catch (ZebraPrinterConnectionException)
+            {
+                updateGuiFromWorkerThread("Unable to connect with printer", Color.Red);
+                disconnect();
+            }
+            catch (Exception)
+            {
+                updateGuiFromWorkerThread("Error communicating with printer", Color.Red);
+                disconnect();
+            }
+
+            printer = null;
+            if (connection != null && connection.IsConnected())
+            {
+                /**
+                 * Get the printer instance and the printer language, then print image
+                 * **/
+                try
+                {
+                    updateGuiFromWorkerThread("Getting printer...", Color.Goldenrod);
+                    Thread.Sleep(1000);
+                    printer = ZebraPrinterFactory.GetInstance(connection);
+                    updateGuiFromWorkerThread("Got Printer, getting Language...", Color.LemonChiffon);
+                    Thread.Sleep(1000);
+                    PrinterLanguage pl = printer.GetPrinterControlLanguage();
+                    updateGuiFromWorkerThread("Printer Language " + pl.ToString(), Color.LemonChiffon);
+                    Thread.Sleep(1000);
+                    updateGuiFromWorkerThread("Formatting Image", Color.AliceBlue);
+                    printer.GetGraphicsUtil().PrintImage(image, 0, 0, 550, 412, false);
+                    updateGuiFromWorkerThread("Printing To " + addressName, Color.Green);
+                    Thread.Sleep(2000);
+                    disconnect();
+                }
+                catch (ZebraPrinterConnectionException)
+                {
+                    updateGuiFromWorkerThread("Unknown Printer Language", Color.Red);
+                    printer = null;
+                    Thread.Sleep(1000);
+                    disconnect();
+                }
+                catch (ZebraPrinterLanguageUnknownException)
+                {
+                    updateGuiFromWorkerThread("Unknown Printer Language", Color.Red);
+                    printer = null;
+                    Thread.Sleep(1000);
+                    disconnect();
+                }
+            }
+            updateButtonFromWorkerThread(true);
+        }
+         /**
+         * Start disconnect thread
+         * **/
+        public Thread disconnect()
+        {
+            Thread t = new Thread(doDisconnect);
+            t.Start();
+            return t;
+        }
+
+        /**
+         * Close connection to printer
+         * **/
+
+        private void doDisconnect()
+        {
+            try
+            {
+                if (connection != null && connection.IsConnected())
+                {
+                    updateGuiFromWorkerThread("Disconnecting...", Color.Honeydew);
+                    
+                    connection.Close();
+                }
+            }
+            catch (ZebraException)
+            {
+                updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red);
+            }
+            Thread.Sleep(1000);
+            updateGuiFromWorkerThread("Not Connected", Color.Red);
+            Thread.Sleep(1000);
+            updateButtonFromWorkerThread(true);
+            connection = null;
+        }
+        /*******************************************************************************
+         * End printer connection methods
+         * ****************************************************************************/
+
+
+        /*******************************************************************************
+         * The following methods update the status bar at top of screen
+         * ****************************************************************************/
+
+        public void updateGuiFromWorkerThread(String message, Color color)
+        {
+            Invoke(new StatusEventHandler(UpdateUI), new StatusArguments(message, color));
+        }
+
+        private delegate void StatusEventHandler(StatusArguments e);
+
+        private void UpdateUI(StatusArguments e)
+        {
+            statusBar.Text = e.message;
+            statusBar.BackColor = e.color;
+        }
+
+        public class StatusArguments : System.EventArgs
+        {
+            public String message;
+            public Color color;
+
+            public StatusArguments(String message, Color color)
+            {
+                this.message = message;
+                this.color = color;
+            }
+        }
+        /*******************************************************************************
+         * End status bar update methods
+         * ****************************************************************************/
+
+        /*******************************************************************************
+         * The following methods enable and disable buttons as needed
+         * ****************************************************************************/
+
+        public void updateButtonFromWorkerThread(bool enabled)
+        {
+            Invoke(new ButtonStatusHandler(UpdateButton), enabled);
+        }
+        private delegate void ButtonStatusHandler(bool enabled);
+        private void UpdateButton(bool enabled)
+        {
+           
+        }
+
+        private void GetImage()
+        {
+            try
+            {
+
+                // Select rotation
+                
+                int angletorotate = 0;
+               
+                if (radioButton90.Checked)
+                {
+                    angletorotate = 90;
+                }
+                else if (radioButton180.Checked)
+                {
+                    angletorotate = 180;
+                }
+                else if (radioButton270.Checked)
+                {
+                    angletorotate = 270;
+                }
+                else
+                {
+                    image = new Bitmap(sFileName);
+                    return;
+                }
+
+                Bitmap mybitmap = new Bitmap(sFileName);
+                // Make the width the height and height the width if the rotation angle is 90 or 270
+                bool aspectRatio = (angletorotate == 90 || angletorotate == 270);
+                int newWidth = (aspectRatio ? mybitmap.Height : mybitmap.Width);
+                int newHeight = (aspectRatio ? mybitmap.Width : mybitmap.Height);
+
+                // Create a Bitmap that will be the rotated Bitmap
+                Bitmap rotatedBitmap = new Bitmap(newWidth, newHeight);
+
+                int WidthMinusOne = newWidth - 1;
+                int HeightMinusOne = newHeight - 1;
+
+                // Lock images into system memory
+                BitmapData myLockedBitmap = mybitmap.LockBits(new Rectangle(0, 0, mybitmap.Width, mybitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+                BitmapData rotatedLockedBitmap = rotatedBitmap.LockBits(new Rectangle(0, 0, rotatedBitmap.Width, rotatedBitmap.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+
+                unsafe
+                {
+                    int* myPtr = (int*)myLockedBitmap.Scan0.ToPointer();
+                    int* rotatedPtr = (int*)rotatedLockedBitmap.Scan0.ToPointer();
+
+                    //Rotate the image
+                        if(angletorotate == 90)
+                        {
+                            for (int y = 0; y < mybitmap.Height; ++y)
+                            {
+                                int destinationX = WidthMinusOne - y;
+                                for (int x = 0; x < mybitmap.Width; ++x)
+                                {
+                                    int sourcePosition = (x + y * mybitmap.Width);
+                                    int destinationY = x;
+                                    int destinationPosition = (destinationX + destinationY * newWidth);
+                                    rotatedPtr[destinationPosition] = myPtr[sourcePosition];
+                                }
+                            }
+                        }
+                        else if(angletorotate == 180)
+                        {
+                            for (int y = 0; y < mybitmap.Height; ++y)
+                            {
+                                int destinationY = (HeightMinusOne - y) * newWidth;
+                                for (int x = 0; x < mybitmap.Width; ++x)
+                                {
+                                    int sourcePosition = (x + y * mybitmap.Width);
+                                    int destinationX = WidthMinusOne - x;
+                                    int destinationPosition = (destinationX + destinationY);
+                                    rotatedPtr[destinationPosition] = myPtr[sourcePosition];
+                                }
+                            }
+                        }
+
+                        else if (angletorotate == 270)
+                        {
+                            for (int y = 0; y < mybitmap.Height; ++y)
+                            {
+                                int destinationX = y;
+                                for (int x = 0; x < mybitmap.Width; ++x)
+                                {
+                                    int sourcePosition = (x + y * mybitmap.Width);
+                                    int destinationY = HeightMinusOne - x;
+                                    int destinationPosition = (destinationX + destinationY * newWidth);
+                                    rotatedPtr[destinationPosition] = myPtr[sourcePosition];
+                                }
+                            }
+                        }
+                        // Unlock images.
+                        mybitmap.UnlockBits(myLockedBitmap);
+                        rotatedBitmap.UnlockBits(rotatedLockedBitmap);
+                        
+                        // set rotated image
+                        image = new Bitmap(rotatedBitmap);
+                }
+
+               
+            }
+            catch (ZebraPrinterConnectionException i)
+            {
+                MessageBox.Show(i.StackTrace);
+            }
+
+        }
+
+        /**
+            * When folder icon is clicked, file explorer window is opened
+            * **/
+        private void pbxFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Let user navigate through the file system and select an image to send to the printer.
+                OpenFileDialog dlgFile = new OpenFileDialog();
+                dlgFile.Filter = "Image Files (*.jpg;*.bmp;*.png)|*.jpg;*.bmp;*.png|All files (*.*)|*.*";
+                dlgFile.FilterIndex = 1;
+                if (dlgFile.ShowDialog() == DialogResult.OK)
+                {
+                    sFileName = dlgFile.FileName;
+                }
+            }
+            catch
+            {
+            }
+        }
+
+    }
+}
