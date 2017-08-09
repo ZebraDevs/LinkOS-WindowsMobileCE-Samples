1e6db7ba73a31063a174c77c45ca5d1f9ea38b1f
Corn <SCorn@zebra.com>
2017-08-09 11:20:34 -0500

    Initial CameraPrintDemo_C# project commit:

---
+++ CameraPrintDemo_C#/CameraPrint/CameraPrintForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CameraPrint
+{
+    partial class CameraPrintForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.CameraPrintButton = new System.Windows.Forms.Button();
+            this.macAddrBox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CameraPrintButton
+            // 
+            this.CameraPrintButton.BackColor = System.Drawing.Color.DimGray;
+            this.CameraPrintButton.Location = new System.Drawing.Point(29, 220);
+            this.CameraPrintButton.Name = "CameraPrintButton";
+            this.CameraPrintButton.Size = new System.Drawing.Size(177, 45);
+            this.CameraPrintButton.TabIndex = 0;
+            this.CameraPrintButton.Text = "Camera Print";
+            this.CameraPrintButton.Click += new System.EventHandler(this.CameraPrintButton_Click);
+            // 
+            // macAddrBox
+            // 
+            this.macAddrBox.Location = new System.Drawing.Point(9, 73);
+            this.macAddrBox.Name = "macAddrBox";
+            this.macAddrBox.Size = new System.Drawing.Size(223, 21);
+            this.macAddrBox.TabIndex = 1;
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.Color.DimGray;
+            this.connectButton.Location = new System.Drawing.Point(9, 114);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(91, 20);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect";
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.BackColor = System.Drawing.Color.DimGray;
+            this.disconnectButton.Location = new System.Drawing.Point(145, 114);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(87, 20);
+            this.disconnectButton.TabIndex = 3;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(9, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 20);
+            this.label1.Text = "Enter MAC Address to connect Printer";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.BackColor = System.Drawing.Color.Red;
+            this.statusLabel.Location = new System.Drawing.Point(0, 2);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(240, 31);
+            this.statusLabel.Text = "Status: Disconnected";
+            // 
+            // CameraPrintForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.macAddrBox);
+            this.Controls.Add(this.CameraPrintButton);
+            this.Menu = this.mainMenu1;
+            this.Name = "CameraPrintForm";
+            this.Text = "CameraPrint";
+            this.Load += new System.EventHandler(this.CameraPrintForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CameraPrintButton;
+        private System.Windows.Forms.TextBox macAddrBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ CameraPrintDemo_C#/CameraPrint/CameraPrintForm.cs
@@ -0,0 +1,197 @@
+﻿/**************************************************************************************************** 
+ * CONFIDENTIAL AND PROPRIETARY 
+ *
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * Zebra Technologies Corporation and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * 
+ * Copyright Zebra Technologies Corporation 2010
+ *
+ * ALL RIGHTS RESERVED 
+ *****************************************************************************************************/
+
+/*********************************************************************************************
+ File:   CameraPrintForm.cs
+
+ Descr:  Contains classes and routines to connect and disconnect mobile device with zebra 
+ * printer using bluetooth connection. This demo application allows the user to take a picture 
+ * with the WinMobile Device's camera and print it to the printer.
+
+ Date: 06/23/2010 
+ 
+ Rev Date: 06/25/2010
+ *********************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.WindowsMobile.Forms;        // Required to use the camera.  Also, need to add reference
+using ZSDK_API.Printer;
+using ZSDK_API.Comm;
+using ZSDK_API.Util.Internal;
+using ZSDK_API.ApiException;
+
+namespace CameraPrint
+{
+    public partial class CameraPrintForm : Form
+    {
+        
+
+        private ZebraPrinterConnection zebraPrinterConnection = null;
+        private String VersionString = "1.0";
+
+        /***************************************************************************************************
+        Desc: Constructor
+        ****************************************************************************************************/
+        public CameraPrintForm()
+        {
+            InitializeComponent();
+
+            statusLabel.Text = "Status: Disconnected.";
+           
+            this.Text = "CameraPrint ";
+            this.Text += VersionString;
+        }
+
+        private void CameraPrintForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        /**********************************************************************************
+        Desc: This sends the selected image file to zebra printer for printing.
+        ***********************************************************************************/
+        private void printImage(String filePath)
+        {
+            if (zebraPrinterConnection != null && macAddrBox.Text.Length == 12)
+            {
+                ZebraPrinter printer = ZebraPrinterFactory.GetInstance(zebraPrinterConnection);
+
+                // Send image to print
+                printer.GetGraphicsUtil().PrintImage(filePath, 0, 0, 550, 412, false);
+            }
+        }
+
+        /**********************************************************************************
+        Desc: This function is called when Camera Print button is clicked. Pops up Camera 
+         * capture dialog box and allows user to take an image and send that image to print.
+        ***********************************************************************************/
+        private void CameraPrintButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("After taking the photo, some devices require exiting the Camera App for the image to be sent to the printer.", "Note:", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+
+            // Opens camera capture dialog box
+            CameraCaptureDialog cam = new CameraCaptureDialog();
+            cam.Owner = this;
+            try
+            {
+                DialogResult result = cam.ShowDialog();
+
+                // Send image for printing
+                if (result == DialogResult.OK)
+                {
+                    printImage(cam.FileName);
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("No Camera Found");
+                return;
+            }
+
+            
+        }
+
+        /****************************************************************************************************
+        Desc: Called when Connect button is clicked. Open a bluetooth connection with printer using MAC address
+        *****************************************************************************************************/
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            //Check validity of mac address
+            if (macAddrBox.Text.Length != 12)
+            {
+                MessageBox.Show("Enter valid MAC address!");
+                return;
+            }
+
+            //Creating a bluetooth connection with zebra printer
+            if (zebraPrinterConnection == null && macAddrBox.Text.Length == 12)
+            {
+                zebraPrinterConnection = new BluetoothPrinterConnection(macAddrBox.Text);
+                statusLabel.Text = "Status: Connecting to..." + macAddrBox.Text;
+                statusLabel.BackColor = System.Drawing.Color.Gold;
+                Application.DoEvents();
+                try
+                {
+                    zebraPrinterConnection.Open();
+                    while (!zebraPrinterConnection.IsConnected()) { }
+                }
+                catch (ZebraPrinterConnectionException)
+                {
+                    statusLabel.Text = "Status: Disconnected.";
+                    statusLabel.ForeColor = System.Drawing.Color.Red;
+                    zebraPrinterConnection = null;
+                    MessageBox.Show("Unable to connect with printer.");
+                    return;
+                }
+                catch (ZebraException)
+                {
+                    statusLabel.BackColor = Color.Red;
+                    statusLabel.Text = "Status : Disconnected.";
+                    zebraPrinterConnection = null;
+                    MessageBox.Show("Unable to connect with printer.\r\nCheck that printer is on.");
+                    return;
+                }
+                catch (Exception)
+                {
+                    statusLabel.BackColor = Color.Red;
+                    statusLabel.Text = "Status : Disconnected.";
+                    zebraPrinterConnection = null;
+                    MessageBox.Show("Connection Failed.");
+                    return;
+                }
+                statusLabel.Text = "Status: Connected to " + macAddrBox.Text;
+                statusLabel.BackColor = System.Drawing.Color.Green;
+            }
+        }
+
+        /*****************************************************************************************************
+        Desc: Called when Disconnect button is clicked. Close the Bluetooth connection with printer.
+        ******************************************************************************************************/
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Close the connection
+                if (zebraPrinterConnection != null)
+                {
+                    zebraPrinterConnection.Close();
+                }
+            }
+            catch (ZebraPrinterConnectionException)
+            {
+                MessageBox.Show("Unable to disconnect with printer.");
+                return;
+            }
+            catch (ZebraException)
+            {
+                MessageBox.Show("Unable to disconnect with printer.\r\nCheck that printer is on.");
+                return;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Disconnect Failed.");
+                return;
+            }
+            zebraPrinterConnection = null;
+            statusLabel.BackColor = Color.Red;
+            statusLabel.Text = "Status : Disconnected.";
+        }
+     }
+}
