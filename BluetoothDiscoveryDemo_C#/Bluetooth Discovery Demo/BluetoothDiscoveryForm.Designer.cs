dfcd1b02a1132bc1bb66305f53e93f086795a6ac
Corn <SCorn@zebra.com>
2017-08-09 11:24:09 -0500

    Initial BluetoothDiscoveryDemo_C# project commit

---
+++ BluetoothDiscoveryDemo_C#/Bluetooth Discovery Demo/BluetoothDiscoveryForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿/********************************************** 
+ * CONFIDENTIAL AND PROPRIETARY 
+ *
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * 
+ * Copyright ZIH Corp. 2010
+ *
+ * ALL RIGHTS RESERVED 
+ ***********************************************
+ File:   BluetoothDiscoverForm.Designer.cs
+
+ Description: Form layout and design for BluetoothDiscoverForm.
+
+ $Revision: 1 $
+ $Date: 2010/06/03 $
+ *******************************************************************************/
+
+namespace Bluetooth_Discovery_Demo
+{
+    partial class BluetoothDiscoveryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.discoveryButton = new System.Windows.Forms.MenuItem();
+            this.connectButton = new System.Windows.Forms.MenuItem();
+            this.discoveredPrintersListBox = new System.Windows.Forms.ListBox();
+            this.statusBar = new System.Windows.Forms.Label();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.discoveryButton);
+            this.mainMenu1.MenuItems.Add(this.connectButton);
+            // 
+            // discoveryButton
+            // 
+            this.discoveryButton.Text = "Discover";
+            this.discoveryButton.Click += new System.EventHandler(this.discoverButtonClicked);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Text = "Connect";
+            this.connectButton.Click += new System.EventHandler(this.connectButtonClicked);
+            // 
+            // discoveredPrintersListBox
+            // 
+            this.discoveredPrintersListBox.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.discoveredPrintersListBox.Location = new System.Drawing.Point(3, 29);
+            this.discoveredPrintersListBox.Name = "discoveredPrintersListBox";
+            this.discoveredPrintersListBox.Size = new System.Drawing.Size(234, 212);
+            this.discoveredPrintersListBox.TabIndex = 0;
+            // 
+            // statusBar
+            // 
+            this.statusBar.BackColor = System.Drawing.Color.Red;
+            this.statusBar.Location = new System.Drawing.Point(4, 4);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(233, 23);
+            this.statusBar.Text = "Not Connected";
+            this.statusBar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(125, 245);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(112, 20);
+            this.disconnectButton.TabIndex = 1;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.Click += new System.EventHandler(this.onDisconnectButtonClicked);
+            // 
+            // BluetoothDiscoveryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.discoveredPrintersListBox);
+            this.Menu = this.mainMenu1;
+            this.MinimizeBox = false;
+            this.Name = "BluetoothDiscoveryForm";
+            this.Text = "Bluetooth Discovery";
+            this.Closing += new System.ComponentModel.CancelEventHandler(this.onFrameClose);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuItem discoveryButton;
+        private System.Windows.Forms.MenuItem connectButton;
+        private System.Windows.Forms.ListBox discoveredPrintersListBox;
+        private System.Windows.Forms.Label statusBar;
+        private System.Windows.Forms.Button disconnectButton;
+    }
+}
+
---
+++ BluetoothDiscoveryDemo_C#/Bluetooth Discovery Demo/BluetoothDiscoveryForm.cs
@@ -0,0 +1,314 @@
+﻿/********************************************** 
+ * CONFIDENTIAL AND PROPRIETARY 
+ *
+ * The source code and other information contained herein is the confidential and the exclusive property of
+ * ZIH Corp. and is subject to the terms and conditions in your end user license agreement.
+ * This source code, and any other information contained herein, shall not be copied, reproduced, published, 
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose except as
+ * expressly permitted under such license agreement.
+ * 
+ * Copyright ZIH Corp. 2010
+ *
+ * ALL RIGHTS RESERVED 
+ ***********************************************
+ File:   BluetoothDiscoverForm.cs
+
+ Description: Form and code demonstrating how to discover nearby devices via
+ * Bluetooth. Code detects all broadcasting Bluetooth devices, not just Zebra
+ * Bluetooth devices. Demo also allows users to connect to Zebra devices via
+ * Bluetooth that have been discovered.
+
+ $Revision: 1 $
+ $Date: 2010/06/03 $
+ *******************************************************************************/
+
+using System;
+using System.Drawing;
+using System.ComponentModel;
+using System.Windows.Forms;
+using ZSDK_API.Discovery;
+using System.Threading;
+using ZSDK_API.ApiException;
+using ZSDK_API.Comm;
+using ZSDK_API.Printer;
+
+namespace Bluetooth_Discovery_Demo
+{
+
+    public partial class BluetoothDiscoveryForm : Form
+    {
+        String macAddress;
+        private ZebraPrinterConnection connection;
+        private ZebraPrinter printer;
+
+        public BluetoothDiscoveryForm()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         * When discoverButton is clicked, start new thread to discover nearby bluetooth
+         * devices
+         * **/
+        private void discoverButtonClicked(object sender, EventArgs e)
+        {
+            if (connection != null && connection.IsConnected())
+            {
+                connection.Close();
+            }
+            updateGuiFromWorkerThread("Discovery in progress...", Color.HotPink);
+            
+            discoveredPrintersListBox.Items.Clear();
+            Thread t = new Thread(doBluetoothDiscovery);
+            t.Start();
+        }
+
+        /**
+         * When connectButton is clicked, start new thread to connect to the mac address
+         * that is selected in the listbox
+         * **/
+        private void connectButtonClicked(object sender, EventArgs e)
+        {
+            macAddress = (String)discoveredPrintersListBox.SelectedItem;
+            Thread t = new Thread(doConnectBT);
+            t.Start();
+        }
+        /**
+         * When disconnectButton is clicked, spawn a thread to disconnect from the
+         * currently connected printer
+         * **/
+        public void onDisconnectButtonClicked(Object sender, EventArgs args)
+        {
+            disconnect();
+        }
+
+        /**
+         * Called when the frame has been closed. Closes the open connection to 
+         * a printer if one exists. 
+         * **/
+        public void onFrameClose(Object sender, CancelEventArgs args)
+        {
+            if (connection != null && connection.IsConnected())
+            {
+                connection.Close();
+                connection = null;
+            }
+        }
+        /**
+         * Use BlutoothDiscoverer class to get an array of printers detected
+         * Display printers in listbox
+         * **/
+        private void doBluetoothDiscovery()
+        {
+            
+            updateButtonFromWorkerThread(false);
+            Thread.Sleep(1000);
+            try
+            {
+                DiscoveredPrinter[] printers = BluetoothDiscoverer.FindPrinters();
+                displayPrinters(printers);
+                updateButtonFromWorkerThread(true);
+            }
+            catch (DiscoveryException ex)
+            {
+                handleException(ex.Message);
+                updateButtonFromWorkerThread(true);
+            }
+        }
+
+        /**
+         * Check that macAddress is valid
+         * Connect to device using BluetoothPrinterConnection class
+         * **/
+
+        private void doConnectBT()
+        {
+            if (connection != null)
+            {
+                doDisconnect();
+            }
+            updateButtonFromWorkerThread(false);
+            if (this.macAddress == null || this.macAddress.Length != 12)
+            {
+                updateGuiFromWorkerThread("Invalid MAC Address", Color.Red);
+                disconnect();
+            }
+            else
+            {
+                updateGuiFromWorkerThread("Connecting... Please wait...", Color.Goldenrod);
+                try
+                {
+                    connection = new BluetoothPrinterConnection(this.macAddress);
+                    connection.Open();
+                    Thread.Sleep(1000);
+                }
+                catch (ZebraPrinterConnectionException)
+                {
+                    updateGuiFromWorkerThread("Unable to connect with printer", Color.Red);
+                    disconnect();
+                }
+                catch (ZebraException)
+                {
+                    updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red);
+                    disconnect();
+                }
+                
+                catch (Exception)
+                {
+                    updateGuiFromWorkerThread("Error communicating with printer", Color.Red);
+                    disconnect();
+                }
+
+
+                /**
+                 * Get printer from ZebraPrinterFactory
+                 * Get printer language
+                 * Thread.Sleep allow statusBar updates to be seen before they change
+                 * **/
+
+                printer = null;
+                if (connection != null && connection.IsConnected())
+                {
+                    try
+                    {
+                        updateGuiFromWorkerThread("Getting printer...", Color.Goldenrod);
+                        Thread.Sleep(1000);
+
+                        printer = ZebraPrinterFactory.GetInstance(connection);
+                        updateGuiFromWorkerThread("Got Printer, getting Language...", Color.LemonChiffon);
+                        Thread.Sleep(1000);
+
+                        PrinterLanguage pl = printer.GetPrinterControlLanguage();
+                        updateGuiFromWorkerThread("Printer Language " + pl.ToString(), Color.LemonChiffon);
+                        Thread.Sleep(1000);
+
+                        updateGuiFromWorkerThread("Connected to " + macAddress, Color.Green);
+                        Thread.Sleep(1000);
+                    }
+                    catch (ZebraPrinterConnectionException)
+                    {
+                        updateGuiFromWorkerThread("Unknown Printer Language", Color.Red);
+                        printer = null;
+                        Thread.Sleep(1000);
+                        disconnect();
+                    }
+                    catch (ZebraPrinterLanguageUnknownException)
+                    {
+                        updateGuiFromWorkerThread("Unknown Printer Language", Color.Red);
+                        printer = null;
+                        Thread.Sleep(1000);
+                        disconnect();
+                    }
+                    updateButtonFromWorkerThread(true);
+                }
+            }
+        }
+
+        /**
+         * Start disconnect thread
+         * **/
+        public void disconnect()
+        {
+            Thread t = new Thread(doDisconnect);
+            t.Start();
+        }
+
+        /**
+         * Close connection to printer
+         * **/
+        private void doDisconnect()
+        {
+            updateButtonFromWorkerThread(false);
+            try
+            {
+                if (connection != null && connection.IsConnected())
+                {
+                    updateGuiFromWorkerThread("Disconnecting...", Color.Honeydew);
+                    
+                    connection.Close();
+                }
+            }
+            catch (ZebraException)
+            {
+                updateGuiFromWorkerThread("COMM Error! Disconnected", Color.Red);
+            }
+            Thread.Sleep(1000);
+            updateGuiFromWorkerThread("Not Connected", Color.Red);
+            connection = null;
+            updateButtonFromWorkerThread(true);
+        }
+
+        /********************************************************************************
+         * The following methods update the discovered printers listbox
+         * *****************************************************************************/
+        private void handleException(String message)
+        {
+            updateGuiFromWorkerThread(message, Color.Red);
+            BeginInvoke(new MyProgressEventsHandler(UpdateDiscoveredPrinters), new object[] { this, new DiscoveredPrinter[] { } });
+        }
+
+        private delegate void MyProgressEventsHandler(object sender, DiscoveredPrinter[] printers);
+
+        private void UpdateDiscoveredPrinters(object sender, DiscoveredPrinter[] printers)
+        {
+            foreach (DiscoveredPrinter printer in printers)
+            {
+                discoveredPrintersListBox.Items.Add(printer.Address);
+            }
+            discoveryButton.Enabled = true;
+        }
+
+        private void displayPrinters(DiscoveredPrinter[] printers)
+        {
+            updateGuiFromWorkerThread("Discovered " + printers.Length + " printers", Color.Green);
+            BeginInvoke(new MyProgressEventsHandler(UpdateDiscoveredPrinters), new object[] { this, printers });
+        }
+
+        /*******************************************************************************
+         * The following methods update the status bar at top of screen
+         * ****************************************************************************/
+
+        public void updateGuiFromWorkerThread(String message, Color color)
+        {
+            Invoke(new StatusEventHandler(UpdateUI), new StatusArguments(message, color));
+        }
+
+        private delegate void StatusEventHandler(StatusArguments e);
+
+        private void UpdateUI(StatusArguments e)
+        {
+            statusBar.Text = e.message;
+            statusBar.BackColor = e.color;
+        }
+
+        public class StatusArguments : System.EventArgs
+        {
+            public String message;
+            public Color color;
+
+            public StatusArguments(String message, Color color)
+            {
+                this.message = message;
+                this.color = color;
+            }
+        }
+
+        /*******************************************************************************
+         * The following methods enable and disable buttons as needed
+         * ****************************************************************************/
+
+        public void updateButtonFromWorkerThread(bool enabled)
+        {
+            Invoke(new ButtonStatusHandler(UpdateButton), enabled);
+        }
+        private delegate void ButtonStatusHandler(bool enabled);
+        private void UpdateButton(bool enabled)
+        {
+            discoveryButton.Enabled = enabled;
+            connectButton.Enabled = enabled;
+            disconnectButton.Enabled = enabled;
+        }
+    }
+
+    
+}
